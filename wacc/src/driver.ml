68d804829746835a03f5ae8fa9f12bbd0a5a141f
Jian Rong <jr2216@matrix38.doc.ic.ac.uk>
2017-10-27 20:33:16 +0100

    update driver

--- wacc/src/driver.ml
+++ wacc/src/driver.ml
@@ -1,44 +1,16 @@
 open Lexing
 open Printf
 open Stack
+open Interpreter
 
 module A = Ast
 
 let usage = "The WACC compiler\nUsage:\n"
 let syntax_error_code = 100
 let semantic_error_code = 200
-type wrapType =
-  | Int of int
-  | Char of char
-  | Bool of bool
-  | String of string
-  | NullType
-  | ListOfElem of A.exp list
-  | DefaultTy
-let stack: wrapType Stack.t = (Stack.create ())
+
 let setup_main = false
 let print_ast = true
-let table = ref (Symbol.empty)
-
-
-let printWrap wrapTy =
-  match wrapTy with
-  | Int(num) -> print_int num
-  | Char(letter) -> print_char letter
-  | Bool(tf) -> if tf then print_string "true" else print_string "false"
-  | String(str) -> print_string str
-  | NullType -> print_string "nil"
-  | DefaultTy -> print_string "Something goes wrong.."
-
-let arithmeticConvert wrapTy =
-  match wrapTy with
-  | Int(num) -> num
-  | _ -> -1                   (*TODO:Need Revision here to make it throw an exception*)
-
-let booleanConvert wrapTy =
-  match wrapTy with
-  | Bool(tf) -> tf
-  | _ -> false
 
 
 
@@ -47,58 +19,6 @@ let pos_lnum_cnum pos =
   let lnum = pos.pos_lnum in
   let cnum = (pos.pos_cnum - pos.pos_bol + 1) in
   (lnum, cnum)
-
-(*Test Code*)
-      let rec compute exp table = match exp with
-        | A.IdentExp (symbol, _) -> compute (Symbol.lookup symbol !table) table
-        | A.LiteralExp(lite, _) -> (match lite with
-              | A.LitString(str) -> String(str)
-              | A.LitBool(boo) -> Bool(boo)
-              | A.LitChar(cha) -> Char(cha)
-              | A.LitInt (num) -> Int(num)
-              | A.LitArray (expList) -> ListOfElem(expList)
-              | A.LitPair (ex1, ex2) -> DefaultTy)
-        | A.BinOpExp (ex1, binop, ex2, _) -> (match binop with
-              | A.PlusOp -> Int(arithmeticConvert (compute ex1 table) + arithmeticConvert(compute ex2 table))
-              | A.MinusOp -> Int(arithmeticConvert (compute ex1 table) - arithmeticConvert(compute ex2 table))
-              | A.TimesOp -> Int(arithmeticConvert (compute ex1 table) / arithmeticConvert(compute ex2 table))
-              | A.DivideOp -> Int(arithmeticConvert (compute ex1 table) / arithmeticConvert(compute ex2 table))
-              | A.GeOp -> Bool (arithmeticConvert (compute ex1 table) >= arithmeticConvert(compute ex2 table))
-              | A.GtOp -> Bool (arithmeticConvert (compute ex1 table) > arithmeticConvert(compute ex2 table))
-              | A.EqOp -> Bool (arithmeticConvert (compute ex1 table) == arithmeticConvert(compute ex2 table))
-              | A.NeOp -> Bool (arithmeticConvert (compute ex1 table) != arithmeticConvert(compute ex2 table))
-              | A.LtOp -> Bool (arithmeticConvert (compute ex1 table) < arithmeticConvert(compute ex2 table))
-              | A.LeOp -> Bool (arithmeticConvert (compute ex1 table) <= arithmeticConvert(compute ex2 table))
-              | A.AndOp -> Bool (booleanConvert (compute ex1 table) && booleanConvert(compute ex2 table))
-              | A.OrOp -> Bool (booleanConvert (compute ex1 table)|| booleanConvert(compute ex2 table))
-              | A.ModOp -> Int(arithmeticConvert (compute ex1 table) mod arithmeticConvert(compute ex2 table)))
-        (*| A.ArrayIndexExp(symbol, explist,_) -> (
-              match (compute (Symbol.lookup symbol !table) table) with
-              | ListOfElem(elems) -> elems;
-              let elem = evalList elems explist in
-              compute elem table;
-            )
-
-      and evalList someList explist = (match explist with
-        | exp::[] -> List.nth someList (arithmeticConvert (compute exp table))
-        | h::r -> List.nth (evalList someList explist) (List.nth someList (arithmeticConvert compute h table) ) )*)
-
-      let rec matchLHS (lhs: A.exp): string = match lhs with
-      | A.IdentExp(symbol, _) -> symbol
-      | A.FstExp(exp, _) -> (match exp with
-        | A.IdentExp(s,_) -> ("fst" ^ (s))
-        | A.ArrayIndexExp(symbol,_,_) -> ("fst[]" ^ (symbol))
-        | A.FstExp(exps,_) -> matchLHS exps
-        | A.SndExp(exps,_) -> matchLHS exps
-        | _ -> Symbol.symbol "what"
-        )
-      | A.SndExp(exp,_) -> (match exp with
-        | A.IdentExp(symbol,_) -> ("snd" ^ (symbol))
-        | A.ArrayIndexExp(symbol,_,_) -> ("snd[]" ^ symbol))
-        | A.FstExp(exps,_) -> matchLHS exps
-        | A.SndExp(exps,_) -> matchLHS exps
-        | _ -> Symbol.symbol "what"
-
 
 
 let handle_syntax_error lexbuf =
@@ -128,32 +48,6 @@ let handle_semantic_error lexbuf =
     exit(semantic_error_code);
   end
 
-  let rec eval singleStmt = (match singleStmt with
-    | A.SeqStmt (stmt, stmtlist) -> (eval stmt; eval stmtlist)
-    | A.SkipStmt(_) ->  ()
-    | A.VarDeclStmt(_,sym,exp,_) ->(
-       table := Symbol.insert sym exp !table;
-      let value = compute (Symbol.lookup sym !table) table in
-      Stack.push value stack;
-      )
-    | A.AssignStmt(lhs, rhs, _) ->(
-      table := Symbol.insert (matchLHS lhs) rhs !table;
-      let rhs = compute (Symbol.lookup (matchLHS lhs) !table) table in
-      Stack.push rhs stack;
-      )
-    | A.PrintStmt(exp, _) -> (
-         Stack.push (compute exp table) stack;
-         let result = Stack.pop stack in
-            printWrap result;)
-    | A.PrintLnStmt(exp, _) ->(
-         Stack.push (compute exp table) stack;
-         let result = Stack.pop stack in
-         printWrap result;
-         print_newline ();
-       )
-    | A.BlockStmt(statment, _) ->
-         eval statment)
-
 let () =
   if Array.length Sys.argv <= 1 then
     begin
@@ -165,7 +59,7 @@ let () =
     let lexbuf = Lexing.from_channel (open_in filename) in
     try
       let (decs, stmt) = Parser.prog Lexer.main lexbuf in
-      let () = eval stmt in exit(0);
+      let () = Interpreter.eval stmt in exit(0);
       (* setup_builtin_functions; *)
       (* let table' = Symbol.new_scope (add_func_declarations table decs) in *)
       (* (\* function declarations *\) *)
