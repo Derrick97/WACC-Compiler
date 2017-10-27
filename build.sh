2a38e53c14167e20510189378e541d33d5cf110b
ethanluoyc <ethanluoyc@gmail.com>
2017-10-27 22:58:15 +0100

    add dependency on menhir.

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+if [ -e "menhir-20170712/build/bin/menhir" ];
+then
+    echo "Menhir exists"
+else
 wget http://gallium.inria.fr/~fpottier/menhir/menhir-20170712.tar.gz
-tar -xvzf menhir-20170712.tar.gz
+tar -xzf menhir-20170712.tar.gz
 cd menhir-20170712/
 mkdir -p `pwd`/build
 make PREFIX=`pwd`/build USE_OCAMLFIND=false all > menhir_build.out
 make PREFIX=`pwd`/build USE_OCAMLFIND=false install >> menhir_build.out
+fi
