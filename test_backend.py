f8657d8db18801957502e86af964568b5bfc27f3
Yicheng Luo <yicheng.luo16@imperial.ac.uk>
2017-10-30 17:30:19 +0000

    refine backend test harness.

--- test_backend.py
+++ test_backend.py
@@ -2,31 +2,41 @@ import os
 import sys
 import subprocess
 
-def test_output():
-    pass
-
-def test_exit():
-    pass
-
 def compile(f):
-    pass
+    subprocess.run(['./wacc/env.byte', f])
+    subprocess.run(['./tools/arm-gcc', f.replace('.wacc', '.s')])
+    out = subprocess.run(['./tools/arm-run', 'a.out'])
+    output = '' if not out.stdout else out.stdout.decode('utf-8')
+    return (output, out.returncode)
 
-if __name__ == '__main__':
-    filename = sys.argv[1]
+def test_run(filename):
     expected = {}
     infile = open(filename, 'rt')
     for line in infile:
         if line.startswith("#"):
             tks = line.split(" ")
-            if tks[1].strip() in ['Output:', 'Exit']:
+            if tks[1].strip() in ['Output:', 'Exit:']:
                 title = tks[1].strip()
                 expected[title] = []
-                sys.stdout.write(tks[1])
                 for l in infile:
                     if not l.startswith("#"):
-                        sys.stdout.write(''.join(expected[title]))
                         break
                     else:
                         ll = ' '.join(l.split(' ')[1:])
                         expected[title].append(ll)
     infile.close()
+    output, retcode = compile(filename)
+    if "Output:" in expected:
+        expected_output = ''.join(expected['Output:'])
+        if "#empty#" in expected_output:
+            assert len(output) == 0
+        else:
+            assert expected_output == output
+
+    if "Exit:" in expected:
+        expected_ret = int(''.join(expected['Exit:']).strip())
+        assert expected_ret == retcode
+
+
+if __name__ == '__main__':
+    test_run(sys.argv[1])
