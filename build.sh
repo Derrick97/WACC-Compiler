07a7f0f2a442d55e11f3b1290e644649a593d1b6
Yicheng Luo <yicheng.luo16@imperial.ac.uk>
2017-11-30 12:09:42 +0000

    update build to use OPAM.

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@
 if ! [ -x "$(command -v opam)" ]; then
   echo 'Install OPAM now ' >&2
   # Download OPAM, install with system compiler
-  wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s ./ system
   export OPAMROOT=`pwd`/opamroot
+  wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s ./ system
   eval `./opam config env`
   export PATH=`pwd`:$PATH
 else
   echo "OPAM exists locally"
 fi
+  opam install -y ppx_deriving ocamlgraph ounit menhir
+
+make -C wacc driver.native
