da26081788c78b87cea1d4bf06e2523b65518521
ethanluoyc <ethanluoyc@gmail.com>
2017-12-01 16:54:24 +0000

    add tests for the garbage collector

--- wacc/src/gc.c
+++ wacc/src/gc.c
@@ -9,9 +9,11 @@
 
 #define UNMARKED 0
 #define MARKED   1
+#define MAX_CAPACITY 2
 
 typedef struct {
   struct node* first_node;
+  int max_capacity;
 } gc_ctx;
 
 gc_ctx* global_ctx;
@@ -20,6 +22,7 @@ void init_gc_ctx(void) {
   printf("[GC] creating GC context\n");
   global_ctx = malloc(sizeof(gc_ctx));
   global_ctx->first_node = NULL;
+  global_ctx->max_capacity = MAX_CAPACITY;
 }
 
 void destroy_gc_ctx(void) {
@@ -33,16 +36,6 @@ enum ty {
   TY_PAIR
 };
 
-struct node {
-  unsigned char marked;
-  enum ty t;
-  struct node* next; /* to form a linked list, we keep track of the next object */
-  union {            /* keeps the payload of the node */
-    int node_int;
-    struct pair*  node_pair;
-  };
-};
-
 struct pair {
   struct node* fst;
   struct node* snd;
@@ -52,41 +45,50 @@ struct array {
   struct data* arr;
 };
 
+struct node {
+  unsigned char marked;
+  enum ty t;
+  struct node* next; /* to form a linked list, we keep track of the next object */
+  union {            /* keeps the payload of the node */
+    int node_int;
+    struct pair node_pair;
+  };
+};
+
 typedef struct node node;
 
-inline void unmark(node* n) {
+void unmark(node* n) {
   n->marked = UNMARKED;
 }
 
-inline void mark(node* n) {
+void mark(node* n) {
   n->marked = MARKED;
 }
 
-void markAll(node* root) {
+void mark_all(node* root) {
   if (root->marked) return;        /* avoid cyclic references */
-  mark(root);
+  root->marked = MARKED;
   switch (root->t) {
   case TY_INT:
     return;
   case TY_PAIR:
-    markAll(root->node_pair->fst); /* TODO no recursion */
-    markAll(root->node_pair->snd);
+    mark_all(root->node_pair.fst); /* TODO no recursion */
+    mark_all(root->node_pair.snd);
     break;
   }
 }
 
 node* allocate(enum ty t) {
+  fprintf(stderr, "allocate\n");
   node* ptr = malloc(sizeof(node));
   switch (t) {
   case TY_INT:
     break;
   case TY_PAIR:
-    ptr->node_pair->fst = malloc(sizeof(node));
-    ptr->node_pair->snd = malloc(sizeof(node));
     break;
   }
+  ptr->t = t;
   ptr->marked = UNMARKED;
-
   /* update global linked list */
   ptr->next = global_ctx->first_node;
   global_ctx->first_node = ptr;
@@ -94,27 +96,46 @@ node* allocate(enum ty t) {
 }
 
 void deallocate(node* node) {
+  fprintf(stderr, "deallocate\n");
   switch (node->t) {
   case TY_INT:
     return;
   case TY_PAIR:
-    deallocate(node->node_pair->fst);
-    deallocate(node->node_pair->snd);
+    deallocate(node->node_pair.fst);
+    deallocate(node->node_pair.snd);
     break;
   }
+  free(node);
 }
 
-void sweepAll(void) {
+void sweep_all(void) {
   node* ptr = global_ctx->first_node;
   while (ptr != NULL) {
+    node* next = ptr->next;
     if (!ptr->marked) {
       deallocate(ptr);
+    } else {
+      ptr->marked = UNMARKED;
     }
-    ptr = ptr->next;
+    ptr = next;
   }
 }
 
-void gc(void) {
-  printf("[GC] garbage collection occurred");
+void gc(node* root) {
   /* TODO */
+  mark_all(root);
+  sweep_all();
+  printf("[GC] garbage collection occurred\n");
+}
+
+int main() {
+  init_gc_ctx();
+  node* n_int0 = allocate(TY_INT);
+  node* n_int1 = allocate(TY_INT);
+  allocate(TY_INT); // Allocate garbage
+  node* n_pair = allocate(TY_PAIR);
+  n_pair->node_pair.fst = n_int0;
+  n_pair->node_pair.snd = n_int1;
+  gc(n_pair);
+  destroy_gc_ctx();
 }
