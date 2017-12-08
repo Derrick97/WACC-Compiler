05174cb7566ae0ae9c20aac31acabc9462b2fb2c
Yicheng Luo <yicheng.luo16@imperial.ac.uk>
2017-12-08 11:46:04 +0000

    update OPAM installer.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v opam)" ]; then
   echo 'Install OPAM now ' >&2
   # Download OPAM, install with system compiler
   export OPAMROOT=`pwd`/opamroot
-  wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s ./ system
+  wget https://raw.github.com/ocaml/opam/latest/shell/opam_installer.sh -O - | sh -s ./ system
   ./opam init --comp system
   eval `./opam config env`
   export PATH=`pwd`:$PATH
